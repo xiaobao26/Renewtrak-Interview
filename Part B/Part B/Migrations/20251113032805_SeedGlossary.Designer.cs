5f354b6ef2512ce8e809c94ae32966ca16fa3450
xiaobao26 <xiaobao.xue@outlook.com>
2025-11-13 14:29:21 +1100

    chore: add seed data migration files

--- Part B/Part B/Migrations/20251113032805_SeedGlossary.Designer.cs
+++ Part B/Part B/Migrations/20251113032805_SeedGlossary.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Part_B.Infrastructure;
 
@@ -11,9 +12,11 @@ using Part_B.Infrastructure;
 namespace Part_B.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20251113032805_SeedGlossary")]
+    partial class SeedGlossary
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ Part B/Part B/Migrations/20251113032805_SeedGlossary.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace Part_B.Migrations
+{
+    /// <inheritdoc />
+    public partial class SeedGlossary : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "GlossaryTerms",
+                columns: new[] { "Id", "Definition", "Term" },
+                values: new object[,]
+                {
+                    { new Guid("3fa85f64-5717-4562-b3fc-2c963f66afa9"), "The ocean floor offshore from the continental margin, usually very flat with a slight slope.", "abyssal plain" },
+                    { new Guid("3fa85f64-5717-4562-b3fc-2c963f66afb6"), "v. To add terranes (small land masses or pieces of crust) to another, usually larger, land mass.", "accrete" },
+                    { new Guid("3fa85f64-5717-4562-b3fc-2c963f66afc7"), "Term pertaining to a highly basic, as opposed to acidic, subtance. For example, hydroxide or carbonate of sodium or potassium.", "alkaline" }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "GlossaryTerms",
+                keyColumn: "Id",
+                keyValue: new Guid("3fa85f64-5717-4562-b3fc-2c963f66afa9"));
+
+            migrationBuilder.DeleteData(
+                table: "GlossaryTerms",
+                keyColumn: "Id",
+                keyValue: new Guid("3fa85f64-5717-4562-b3fc-2c963f66afb6"));
+
+            migrationBuilder.DeleteData(
+                table: "GlossaryTerms",
+                keyColumn: "Id",
+                keyValue: new Guid("3fa85f64-5717-4562-b3fc-2c963f66afc7"));
+        }
+    }
+}
